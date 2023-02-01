c0509175dbfddf4cc842ae06c7074a1573913baf
prolegosi <propastogorie@yandex.ru>
2023-02-01 23:15:17 +0300

    1.2 request 70%

--- main.py
+++ main.py
@@ -6,33 +6,89 @@ from settings import TG_TOKEN
 import pendulum
 import sqlite3
 import requests
-bot = telebot.TeleBot(TG_TOKEN)
+import numpy as np
+import pickle
 
+bot = telebot.TeleBot(TG_TOKEN)
+with open('files/USD_RUB_model.pkl', 'rb') as file:
+    model = pickle.load(file)
+file.close()
 
 def create_graph():
-  days = pendulum.now('Europe/Moscow').format('YYYY-MM-DD')
-  con = sqlite3.connect('data.db')
+    now = pendulum.now('Europe/Moscow').format('YYYY-MM-DD')
 
-  cur = con.cursor()
+    con = sqlite3.connect('data.db')
+    cur = con.cursor()
 
-  cur.execute(""" SELECT MAX(days) FROM USD_RUB_data""")
+    cur.execute(""" SELECT MAX(days) FROM USD_RUB_data""")
+    row = cur.fetchall()
+    db_date = row[0][0]
 
-  row = cur.fetchall()
-  db_date = row[0][0]
+    print(now, db_date)
+    if now > db_date:
+    # открываем данные для нормализации
+        m_m_open = open('files/min_max.txt', 'r')
+        val = m_m_open.read()
+        m_m_open.close()
+        min_max_lst = list(map(float, val.split()))
 
-  if days != db_date:
-    url = 'https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=USD&to_symbol=RUB&apikey=W284OCJ6Y1UZJK7P'
-    r = requests.get(url)
-    data = r.json()
+    # получаем данные с API
+        url = 'https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=USD&to_symbol=RUB&apikey=W284OCJ6Y1UZJK7P'
+        r = requests.get(url)
+        data = r.json()
+
+        counter = 0
+        for i in data['Time Series FX (Daily)'].items():
+            done = True
+            d = dict(i[1])
+            lst = []
+            if db_date > now:
+                print('чаво?')
+                # написать ретурн
+                break
+            for j in d.items():
+                lst.append(float(j[1]))
+            # Нормализация
+            lst_norm = [(x - min_max_lst[0]) / (min_max_lst[1] - min_max_lst[0]) for x in lst]
+
+            days = i[0]
+            if days == now:
+                done = False
+            opening, high, low, closes = tuple(lst_norm)
+            close_c = lst[3]
+            X = np.array(lst_norm).reshape(1, -1)
+            # Создание предсказания
+            predict_after = model.predict(X)
+
+            if counter > 0:
+                predict = round(float(predict_before), 2)
+            else:
+                cur.execute(f""" SELECT predict FROM USD_RUB_data WHERE days = '{db_date}'""")
+                pred = cur.fetchall()
+                predict = pred[0][0]
+                print(predict)
 
 
-  r.close()
 
-  print(days)
-  cur.close()
+            print(f'{days}, {opening}, {high}, {low}, {closes}, {close_c}, {predict}')
+
+            try:
+                cur.execute(f"""INSERT INTO USD_RUB_data VALUES('{days}', {opening}, {high}, {low}, {closes}, {close_c}, {predict}) """)
+            except sqlite3.IntegrityError:
+                print('дата уже существует')
+
+            predict_before = predict_after
+            if not done:
+                print('ммм???')
+                break
+            counter += 1
+        days = pendulum.tomorrow().format('YYYY-MM-DD')
+
+        #cur.execute(f"""INSERT INTO USD_RUB_data (days, predict) VALUES('{days}',{round(float(predict_after), 2)}) """)
 
 
-
+    con.commit()
+    cur.close()
 
 
 """
